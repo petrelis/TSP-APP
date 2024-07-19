e7bd9c2ca979782417b4e7462f3ae9b3678aa3a4
petrelis <vistinas108@gmail.com>
2024-07-19 14:17:39 +0300

    Configuration changes

--- TSP APP/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TSP APP.AssemblyInfo.cs
+++ TSP APP/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TSP APP.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TSP APP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+470a79aa4125ba823914422718fefbd2bec04835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+97621b8d8de852f9f76171f4819275c51bd1e5c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TSP APP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TSP APP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
